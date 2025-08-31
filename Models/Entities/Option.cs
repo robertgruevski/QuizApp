71ab5f9aa6dcbff650b5fda231fd84e3f66eaa2d
robertgruevski <robertgruevski@outlook.com>
2025-08-31 15:39:29 -0400

    Model set up

---
+++ Models/Entities/Option.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizApp.Models.Entities
+{
+	public record Option
+	{
+		public Guid Id { get; init; }
+		public required string Text { get; init; }
+
+		// Relationship
+		public Guid QuestionId { get; init; }
+	}
+}
---
+++ Models/Entities/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizApp.Models.Entities
+{
+	public record Question
+	{
+		public Guid Id { get; init; }
+		public required string Text { get; init; }
+
+		// Options
+		public required List<Option> Options { get; set; }
+		public required Guid CorrectOption { get; init; }
+	}
+}
